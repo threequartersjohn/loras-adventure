c54cc950e19d72a25e99f1a636f22d63c5fd3c57
threequartersjohn <threequartersjohn@gmail.com>
2019-01-18 11:01:02 +0000

    enemy e background

---
+++ Assets/Scripts/CameraPositionControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositionControl : MonoBehaviour {
+
+    //inputs
+    [SerializeField]
+    private string VerticalCameraOffsetInput;
+
+    //Camera offset and limits
+    [SerializeField]
+    float CameraOffsetValue;
+    [SerializeField]
+    float CameraOffsetSpeed;
+    Vector3 CameraOffset;
+    const float LOWER_LIMIT = 0;
+    
+    
+    //Player variables
+    Transform PlayerTransform;
+    float PlayerPosition = 0;
+    float OriginalPlayerPosition = 0;
+
+	// Use this for initialization
+	void Start () {
+        PlayerTransform = GameObject.Find("Player").transform;
+        OriginalPlayerPosition = PlayerTransform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        ManageInputs();
+
+        PlayerPosition = PlayerTransform.position.y;
+        Vector3 newCameraPosition = new Vector3(this.transform.position.x, 
+                                                Mathf.Clamp(PlayerPosition, 0, PlayerPosition), 
+                                                this.transform.position.z);
+
+        this.transform.position = newCameraPosition + CameraOffset;
+
+	}
+
+    private void ManageInputs()
+    {
+        float offset = Input.GetAxis(VerticalCameraOffsetInput);
+
+        if (offset != 0)
+        {
+            CameraOffset.y = CameraOffsetValue * offset * CameraOffsetSpeed ;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField]
+    float HorizontalLimit;
+    [SerializeField]
+    float HorizontalSpeed;
+
+    bool MoveLeft = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleDirectionChange();
+        HandleMovement();
+	}
+
+    private void HandleDirectionChange()
+    {
+
+        print(Mathf.Abs(this.transform.position.x));    
+        if (Mathf.Abs(this.transform.position.x) > HorizontalLimit)
+        { 
+            print("Direction change");
+            MoveLeft = !MoveLeft;
+            flipObjectHorizontal();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        print("movement change");
+
+        Vector3 newPosition = this.transform.position;
+        if (MoveLeft == true) newPosition.x -= HorizontalSpeed * Time.deltaTime;
+        else newPosition.x += HorizontalSpeed * Time.deltaTime;
+
+        this.transform.position = newPosition;
+        print(this.transform.position.x);
+    }
+
+    private void flipObjectHorizontal()
+    {
+        print("inverted");
+        Vector3 OriginalLocalScale = this.transform.localScale;
+        OriginalLocalScale.x *= -1;
+        this.transform.localScale = OriginalLocalScale;
+
+    }
+}
